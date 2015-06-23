390a24882f4c2367df507074550a4f2de14f8d5a
Philipp Kuinke <philipp@comtip.net>
2015-06-23 17:50:32 +0200

    small changes

--- curseInfo.js
+++ curseInfo.js
@@ -1,8 +1,10 @@
 var auth = require("./l2pAuth");
 auth.setClientID('Se8Uit5Hd244cltskaVUpHFVXaZ6exvKVTKsKhKTH70yTlPlWoHHUR5RFsy30nV.app.rwth-aachen.de')
 auth.getTokens("TVJpNoNT4k5l9Sk7AQowsPkaBKGjWf8R",function(response){
-	//console.log(response);
+	console.log("TOKEN RETURN:")
+	console.log(response);
 	auth.curseinfo(response.access_token,function(response){
+		console.log("COURSE INFO RETURN:")
 		console.log(response);
 	})
 });
